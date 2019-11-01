f6461926dc911c729af718381b173c81dd2f9169
Hugo <hpeters1024.nl>
2019-11-01 17:10:08 +0100

    watch: all files except vim swaps

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -3,7 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo $DIR
 cd $DIR
 echo "Watching changes to design..."
-while chronic inotifywait ../design -r -e modify; do 
+while chronic inotifywait ../ -r -e modify -e create --exclude '.*.swp'; do 
+  echo building...
   cd ..;
   chronic pipenv run blog build --debug;
   cd scripts;
